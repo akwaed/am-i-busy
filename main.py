d67d656caacbe7436c9b5642fd75af6c5ef7d3a8
akwaed <akwaed@gmail.com>
2023-08-27 00:32:14 -0400

    did modify link to a calender as apple weblink is bugging

--- main.py
+++ main.py
@@ -1,39 +1,30 @@
-# Requires ics to work!!
 import requests
 from ics import Calendar
-from datetime import datetime
+from datetime import datetime, date
 
-def is_user_busy(webcal_link):
-    try:
-        # Fetch iCalendar data from the webcal link
-        response = requests.get(webcal_link)
-        response.raise_for_status()
+# URL of the iCal feed
+ical_url = "https://uk.instructure.com/feeds/calendars/user_M8AICdwjOhquql0ZIleTQblvXr5UOrAztDCed5oK.ics"
+ical_url2 = "webcal://p123-caldav.icloud.com/published/2/ODA1MDc3NzgyODA1MDc3N84UnVcJslljZ5hZAAslbSfdaG5rUr-dr0AOaue246bmeiYj3ffRxtW3CZhOloHeGEnqRv3s4hMJMaetp-IreMo"
+# Get the iCal data from the URL
+response = requests.get(ical_url2)
+ical_data = response.text
 
-        # Parse the iCalendar data
-        cal = Calendar(response.text)
+# Parse the iCal data
+cal = Calendar(ical_data)
 
-        # Get current datetime
-        now = datetime.now()
+# Choose a specific date for which you want to see events
+target_date = date.today()  # You can change this to any date
 
-        # Check if the user is currently busy
-        for event in cal.events:
-            if event.begin <= now <= event.end:
-                return True
+# Filter events for the chosen date
+events_on_target_date = [event for event in cal.events if event.begin.date() == target_date]
 
-        return False
-
-    except Exception as e:
-        print("An error occurred:", e)
-        return False
-
-def main():
-    webcal_link = 'https://p123-caldav.icloud.com/published/2/ODA1MDc3NzgyODA1MDc3N84UnVcJslljZ5hZAAslbSfdaG5rUr-dr0AOaue246bmeiYj3ffRxtW3CZhOloHeGEnqRv3s4hMJMaetp-IreMo'
-    busy = is_user_busy(webcal_link)
-
-    if busy:
-        print("User is currently busy.")
-    else:
-        print("User is currently not busy.")
-
-if __name__ == "__main__":
-    main()
+# Display the events for the chosen date
+if events_on_target_date:
+    print(f"Events on {target_date}:")
+    for event in events_on_target_date:
+        print(f"Title: {event.name}")
+        print(f"Start Time: {event.begin}")
+        print(f"End Time: {event.end}")
+        print("=" * 30)
+else:
+    print(f"No events found on {target_date}.")
